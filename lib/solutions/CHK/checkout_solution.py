d70d507ef2a1b7561e8714a89eb676067d71e41a
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:39:34 +0000

    Mon Apr 08 14:39:34 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,26 +1,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
-def remove_letters(letter, list):
-    for i in list:
-	    if(i==letter):
-		    list.remove(i)
-
 def checkout(skus):
 
         [x, y] = [(list(skus).count('A') // 3), (list(skus).count('A') % 3)]
         [i, j] = [(list(skus).count('B') // 2), (list(skus).count('B') % 2)]
         total = (x*130 + y*50) + (i*45 + y*30)
-        try:
-            remove_letters('A', list(skus))
-        except:
-            pass
-        try:
-            remove_letters('B', list(skus))
-        except:
-            pass
         map = {'C':20, 'D':15}
-        total += sum([map[x] for x in list(skus)])
+        print(list(skus))
+        total += sum([map[x] for x in skus])
         return total
+
 
 
 
