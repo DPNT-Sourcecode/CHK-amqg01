f790be1ab4f84d91d3a5d5d9ae8de35bc4608a82
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:24:34 +0000

    Mon Apr 08 14:24:34 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,7 @@
 def checkout(skus):
     map = {'A':50, 'B':30, 'C':20, 'D':15}
 
-    return sum([map[x] for x in list(skus)])
+    sum([map[x] for x in list(skus)])
 
     [x, y] = [(list(skus).count('A') // 3), (list(skus).count('A') % 3)]
     [i, j] = [(list(skus).count('B') // 2), (list(skus).count('B') % 2)]
-
-
-
-
--- lib/solutions/CHK/test_chk.py
+++ lib/solutions/CHK/test_chk.py
@@ -3,5 +3,3 @@ from checkout_solution import *
 
 def test_one():
     assert(checkout('AA') == 100)
-
-
