f58c8be00e363194f3288624b25e6b454a175d0f
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 15:04:07 +0000

    Mon Apr 08 15:04:07 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,13 +2,12 @@
 # skus = unicode string
 def checkout(skus):
     map = {'A':50,'B':30,'C':20,'D':10,'E':150,'F':45}
-
     [x, y] = [(list(skus).count('A') // 3), (list(skus).count('A') % 3)]
     [i, j] = [(list(skus).count('B') // 2), (list(skus).count('B') % 2)]
     [c, d] = [list(skus).count('C'), list(skus).count('D')]
     skus = (x*'E'+y*'A'+i*'F'+j*'B'+c*'C'+d*'D')
-    print(skus)
-    print(sum([map[x] for x in list(skus)]))
+    return(sum([map[x] for x in list(skus)]))
+
 
 
 
