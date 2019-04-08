60042e9e8745e3c955254274401437ce641d0518
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:12:34 +0000

    Mon Apr 08 14:12:34 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,7 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     map = {'A':50, 'B':30, 'C':20, 'D':15}
+    return sum([map[x] for x in list(skus)])
+
 
--- lib/solutions/CHK/test_chk.py
+++ lib/solutions/CHK/test_chk.py
@@ -1,5 +1,7 @@
 import pytest
+from checkout_solution import *
 
 def test_one():
     assert(checkout('AA') == 100)
+
 
