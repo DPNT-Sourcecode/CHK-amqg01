f684405f2d0b8ab483c73e8a5ed1f8ccbb75e81b
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 15:46:04 +0000

    Mon Apr 08 15:46:04 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,14 +6,17 @@ def checkout(skus):
             skus = (list(skus))
             A = skus.count('A')
             map = {'A':50,'B':30,'C':20,'D':15,'E':40,'Z':130,'Y':45, 'X':200}
-            if skus.count('A') % 5 == 0 and skus.count('A')
-            [x, y, what, the] = [(skus.count('A') // 3), (skus.count('A') % 3), (skus.count('A') // 5), (skus.count('A') % 5)]
+            if A % 5 == 0 and A > 0:
+                [five_a , y] = [A // 5, A % 5]
+            else:
+                [x, y] = [A // 3, A % 3]
             [i, j] = [(skus.count('B') // 2), (skus.count('B') % 2)]
             [c, d] = [skus.count('C'), skus.count('D')]
-            skus = (x*'Z'+y*'A'+i*'Y'+j*'B'+c*'C'+d*'D'+what*'X'+the*'A')
+            skus = (x*'Z'+y*'A'+i*'Y'+j*'B'+c*'C'+d*'D'+five_a*'X')
             return(sum([map[x] for x in list(skus)]))
         else:
             return(-1)
     else:
         return 0
+
 
