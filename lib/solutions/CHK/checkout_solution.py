7fc3f0d9eb715638329998d43db2a87c163f1129
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 15:46:41 +0000

    Mon Apr 08 15:46:41 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,8 +8,10 @@ def checkout(skus):
             map = {'A':50,'B':30,'C':20,'D':15,'E':40,'Z':130,'Y':45, 'X':200}
             if A % 5 == 0 and A > 0:
                 [five_a , y] = [A // 5, A % 5]
+                x = 0
             else:
                 [x, y] = [A // 3, A % 3]
+                five_a = 0
             [i, j] = [(skus.count('B') // 2), (skus.count('B') % 2)]
             [c, d] = [skus.count('C'), skus.count('D')]
             skus = (x*'Z'+y*'A'+i*'Y'+j*'B'+c*'C'+d*'D'+five_a*'X')
@@ -18,5 +20,6 @@ def checkout(skus):
             return(-1)
     else:
         return 0
+
 
 
