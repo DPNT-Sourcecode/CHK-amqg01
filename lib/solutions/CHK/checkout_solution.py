4cda884adc3b017dd0bad53ee0fada7c1e8473b6
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 17:11:03 +0000

    Mon Apr 08 17:11:03 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,9 +5,9 @@ def checkout(skus):
         if skus.isupper():
             skus = (list(skus))
             A, C, D, E, F = skus.count('A'), skus.count('C'), skus.count('D'), skus.count('E'), skus.count('F')
-            prices = {'A':50,'B':30,'C':20,'D':15,'E':40,'F':10,'G':,'H':,'I':,
-                        'J':,'K':,'L':,'M':,'N':,'O':,'P':,'Q':,'R':,'S':,'T':,
-                        'U':,'V':,'W':,'X','Y':,'Z':,
+            prices = {'A':50,'B':30,'C':20,'D':15,'E':40,'F':10,'G':20,'H':10,'I':35,
+                        'J':60,'K':80,'L':90,'M':15,'N':40,'O':10,'P':50,'Q':30,'R':50,
+                        'S':30,'T':20,'U':40,'V':50,'W':20,'X':90,'Y':10,'Z':50,
                         'a':130,'b':45, 'c':200}
 
             #A LOGIC
@@ -43,6 +43,7 @@ def checkout(skus):
             return(-1)
     else:
         return 0
+
 
 
 
