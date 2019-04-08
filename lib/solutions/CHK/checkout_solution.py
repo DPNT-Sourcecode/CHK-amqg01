32432bf312a297dc91626bcd273053502d441856
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:44:11 +0000

    Mon Apr 08 14:44:11 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,15 +1,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-
     try:
         [x, y] = [(list(skus).count('A') // 3), (list(skus).count('A') % 3)]
         [i, j] = [(list(skus).count('B') // 2), (list(skus).count('B') % 2)]
-        [c, d] = [list(skus),count('C'), list(skus),count('D')]
+        [c, d] = [list(skus).count('C'), list(skus).count('D')]
         total = (x*130 + y*50) + (i*45 + y*30) + (c*20 + d*15)
-        return total
+        print(total)
     except:
-        -1 
+        -1
+
 
 
 
