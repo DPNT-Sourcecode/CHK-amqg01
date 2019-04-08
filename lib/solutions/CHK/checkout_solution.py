bfaed04389a345f5ca1996a4cfcc09477f903a9c
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:44:35 +0000

    Mon Apr 08 14:44:35 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,9 +6,10 @@ def checkout(skus):
         [i, j] = [(list(skus).count('B') // 2), (list(skus).count('B') % 2)]
         [c, d] = [list(skus).count('C'), list(skus).count('D')]
         total = (x*130 + y*50) + (i*45 + y*30) + (c*20 + d*15)
-        print(total)
+        return total 
     except:
         -1
+
 
 
 
