0ab11895c2485e943be069724731c4bedcb1a14a
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 16:40:02 +0000

    Mon Apr 08 16:40:02 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,7 @@ def checkout(skus):
         if skus.isupper():
             skus = (list(skus))
             A, C, D, E  = skus.count('A'), skus.count('C'), skus.count('D'), skus.count('E')
-            map = {'A':50,'B':30,'C':20,'D':15,'E':40,'Z':130,'Y':45, 'X':200}
+            prices = {'A':50,'B':30,'C':20,'D':15,'E':40,'F':10,'Z':130,'Y':45, 'X':200}
 
             #A LOGIC
             five_a, x, y = 0, 0, 0
@@ -18,7 +18,7 @@ def checkout(skus):
                 else:
                     [x, y] = [y // 3, y % 3]
 
-            #EE LOGIC
+            #E LOGIC
             if 'B' in skus:
                 for i in range(E // 2):
                     skus.remove('B')
@@ -31,9 +31,10 @@ def checkout(skus):
 
             #Calculate Total
             skus = (x*'Z'+y*'A'+i*'Y'+j*'B'+C*'C'+D*'D'+five_a*'X'+E*'E')
-            return(sum([map[x] for x in list(skus)]))
+            return(sum([prices[x] for x in list(skus)]))
         else:
             return(-1)
     else:
         return 0
+
 
