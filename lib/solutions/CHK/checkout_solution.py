757298c26bcbc8ddd04b657ed702ec34e9dcf55a
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:18:34 +0000

    Mon Apr 08 14:18:34 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,11 @@
 # skus = unicode string
 def checkout(skus):
     map = {'A':50, 'B':30, 'C':20, 'D':15}
+
     return sum([map[x] for x in list(skus)])
+
+    if list(skus).count('A') % 3 == 0:
+        
+
 
 
