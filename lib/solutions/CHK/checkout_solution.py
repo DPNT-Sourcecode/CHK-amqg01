7fd58fb70519067245a8e382815f9642e4f657a9
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:42:34 +0000

    Mon Apr 08 14:42:34 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,13 +2,15 @@
 # skus = unicode string
 def checkout(skus):
 
+    try:
         [x, y] = [(list(skus).count('A') // 3), (list(skus).count('A') % 3)]
         [i, j] = [(list(skus).count('B') // 2), (list(skus).count('B') % 2)]
-        total = (x*130 + y*50) + (i*45 + y*30)
-        map = {'C':20, 'D':15}
-        print(list(skus))
-        total += sum([map[x] for x in skus])
+        [c, d] = [list(skus),count('C'), list(skus),count('D')]
+        total = (x*130 + y*50) + (i*45 + y*30) + (c*20 + d*15)
         return total
+    except:
+        -1 
+
 
 
 
