d6a019772f8b1093165b4a288e2c13a660bdd0c4
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 15:37:04 +0000

    Mon Apr 08 15:37:04 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,13 +3,17 @@
 def checkout(skus):
     if skus:
         if skus.isupper():
+            skus = (list(skus))
+            A = skus.count('A')
             map = {'A':50,'B':30,'C':20,'D':15,'E':40,'Z':130,'Y':45, 'X':200}
-            [x, y, what, the] = [(list(skus).count('A') // 3), (list(skus).count('A') % 3), (list(skus).count('A') // 5), (list(skus).count('A') % 5)]
-            [i, j] = [(list(skus).count('B') // 2), (list(skus).count('B') % 2)]
-            [c, d] = [list(skus).count('C'), list(skus).count('D')]
+            if skus.count('A') % 5 == 0 and skus.count('A')
+            [x, y, what, the] = [(skus.count('A') // 3), (skus.count('A') % 3), (skus.count('A') // 5), (skus.count('A') % 5)]
+            [i, j] = [(skus.count('B') // 2), (skus.count('B') % 2)]
+            [c, d] = [skus.count('C'), skus.count('D')]
             skus = (x*'Z'+y*'A'+i*'Y'+j*'B'+c*'C'+d*'D'+what*'X'+the*'A')
             return(sum([map[x] for x in list(skus)]))
         else:
             return(-1)
     else:
         return 0
+
